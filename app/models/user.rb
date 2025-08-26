88597e74eca77c01550e65c8766e46ed8301c783
gabalzate <gabriel.andres.alzate@gmail.com>
2025-08-26 17:44:38 -0500

    Finalice la configuración de gemas básicas, listo apra pasar a vista y controlador de inicio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Relación con Workshop: un usuario puede ser administrador de muchos talleres
   has_many :workshops, foreign_key: :admin_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_26_151015) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_220328) do
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id", null: false
     t.integer "recipient_id", null: false
@@ -160,6 +160,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_26_151015) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "workshops", force: :cascade do |t|
